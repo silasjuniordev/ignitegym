b3180a36a221f04bbeca2f092a5da71ea413870f
Silas Junior <silasjuniordev@hotmail.com>
2024-05-13 09:48:37 -0300

    Implementing Image Picker on Profile Page

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 import { TouchableOpacity } from "react-native";
 import { useState } from "react";
 import { Input } from "@components/Input";
@@ -10,6 +12,44 @@ const PHOTO_SIZE = 33
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false)
+    const [userPhoto, setUserPhoto] = useState('https://github.com/silasjuniordev.png')
+
+    const toast = useToast()
+
+    async function handleUserPhotoSelect() {
+        setPhotoIsLoading(true)
+
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true
+            })
+    
+            if (photoSelected.canceled) {
+                return
+            }
+    
+            if (photoSelected.assets[0].uri)  {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)    
+
+                if(photoInfo.exists && (photoInfo.size / 1024 / 1024) > 5) {
+                    return toast.show({
+                        title: 'Esse imagem e muito grande. Escolha uma de ate 5MB',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    })
+                }
+
+                setUserPhoto(photoSelected.assets[0].uri)
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setPhotoIsLoading(false)
+        }
+    }
 
     return (
         <VStack flex={1}>
@@ -29,13 +69,13 @@ export function Profile() {
                         />
                         :
                         <UserPhoto 
-                            source={{ uri: 'https://github.com/silasjuniordev.png' }}
+                            source={{ uri: userPhoto }}
                             alt="Imagem do usuário"
                             size={PHOTO_SIZE}
                         />
                     }
 
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
                             Alterar foto
                         </Text>
